acca80861ea0fe29359ff5d84b083bdc0e339937
Lxpak <73869778+Lxpak@users.noreply.github.com>
2023-08-31 11:54:34 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1883,11 +1883,18 @@ out_ret:
 	return retval;
 }
 
+//addcode start
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+            void *envp, int *flags);
+//addcode end
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+    //addcode
+    ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+    //addcode end
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
